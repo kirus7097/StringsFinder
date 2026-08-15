cf10dc9085984cb55ccda89ceba937b01d1cbb6d
Filp Winkiel <67429383+kirus7097@users.noreply.github.com>
2026-08-15 17:33:04 +0200

    Refactor tryOpenAndReadMapsFile function
    Refactor tryOpenAndReadMapsFile to return void and handle memory regions.

--- stringfinder.c
+++ stringfinder.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
 #include <sys/types.h>
+#include <inttypes.h>
+
+typedef struct {
+    uintptr_t start;
+    uintptr_t end;
+    char permissions[5];
+} MemoryRegion;
 
 static pid_t checkPID() {
     pid_t pid;
@@ -19,20 +28,37 @@ static bool processExists(pid_t pid) {
     else return false;
 }
 
-static char* tryOpenAndReadMapsFile(pid_t pid) {
+static void tryOpenAndReadMapsFile(pid_t pid) {
     char path[256];
     snprintf(path, sizeof(path), "/proc/%d/maps", pid);
     FILE* file = fopen(path, "r");
     if (file == NULL) {
         perror("fopen");
-        return NULL;
+        return;
     }
 
     char content[8192];
+    unsigned int region_count = 0;
+    MemoryRegion region[100];
+
     while (true) {
+        MemoryRegion current;
+
         if (fgets(content, sizeof(content), file) == NULL) break;
-        printf("%s", content);
+        if (sscanf(content, "%" SCNxPTR "-%" SCNxPTR " %4s", &region[region_count].start, &region[region_count].end, region[region_count].permissions) != 3) {
+            fclose(file);
+            printf("Failed to read addresses or parse them");
+            return;
+        }
+
+        if (region[region_count].permissions[0] == 'r') {
+            printf("Start address: %" PRIxPTR "\n", region[region_count].start);
+            printf("End address: %" PRIxPTR "\n", region[region_count].end);
+            printf("Permissions: %s\n", region[region_count].permissions);
+            region_count++;
+        }
     }
+    fclose(file);
 }
 
 int main(void) {
